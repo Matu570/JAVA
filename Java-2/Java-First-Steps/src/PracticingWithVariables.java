390b5c2cd75d7031e6dd4047b40922158e2e8a24
Matias <matiasbueno@hotmail.com.ar>
2023-02-18 00:31:54 -0300

    Practicing with java

---
+++ Java-2/Java-First-Steps/src/PracticingWithVariables.java
@@ -0,0 +1,22 @@
+
+public class PracticingWithVariables {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		
+		int old=31;
+		System.out.println("I have " + old + " years old");
+		
+		double salary=150.50;
+		System.out.println("My salary is " + salary + " dollars");
+		
+		System.out.println("The middle of my salary is " + salary /2 + " dollars");
+
+		int division=old/3;
+		
+		System.out.println(division);
+
+		
+
+		
+	}
+}
