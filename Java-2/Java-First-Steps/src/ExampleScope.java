5891fb040330dd10b89a2257a6d9c73d0d4c7b1a
Matias <matiasbueno@hotmail.com.ar>
2023-02-20 20:46:08 -0300

    Practice about scope

---
+++ Java-2/Java-First-Steps/src/ExampleScope.java
@@ -0,0 +1,29 @@
+
+public class ExampleScope {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		int age = 21;
+		int peopleAmount = 2;
+		
+		boolean couple;
+		
+		if(peopleAmount>1) {
+			couple=true;
+		}else {
+			couple=false;
+		}
+		
+		boolean canEnter=age >= 18 && couple;
+		
+		System.out.println("Tha value of de condition is " + couple);
+
+		if (canEnter) {
+			System.out.println("You can enter");
+			System.out.println("Welcome!");
+		} else {
+			System.out.println("You cannot enter");
+		}
+	}
+	
+}
