431aa9455a986b7a3ff0dfff3fc4b8db561cbd8e
Matias <matiasbueno@hotmail.com.ar>
2023-02-20 18:52:01 -0300

    Uploading TestCharacters file

---
+++ Java-2/Java-First-Steps/src/TestCharacters.java
@@ -0,0 +1,19 @@
+
+public class TestCharacters {
+	public static void main(String[] args) {
+		char letter='a';
+		System.out.println(letter);
+		
+		char charValue=65;
+		System.out.println(charValue);
+		
+		charValue=(char) (charValue+1);
+		System.out.println(charValue);
+		
+		String phrase="Alura tecnologic online courses";
+		System.out.println(phrase);
+		phrase=phrase + 2020;
+		System.out.println(phrase);
+
+	}
+}
