a27833a0236eee914856d08f035b62f62505a2b2
Matias <matiasbueno@hotmail.com.ar>
2023-02-20 18:17:13 -0300

    Upload files

---
+++ Java-2/Java-First-Steps/src/VatiablesValuesExample.java
@@ -0,0 +1,17 @@
+
+public class VatiablesValuesExample {
+	public static void main(String[] args) {
+		int num1=5;
+		int num2=9;
+		System.out.println(num2);
+		
+		num2=num1;
+		System.out.println(num2);
+		
+		num1=88;
+		System.out.println(num2);
+
+		
+		
+	}
+}
---
+++ Java-2/Java-First-Steps/src/characterExamples.java
@@ -0,0 +1,17 @@
+
+public class characterExamples {
+	public static void main(String[] args) {
+		char character='a';
+		System.out.println(character);
+		character=92;
+		System.out.println(character);
+		char secondCharacter=(char) (character+1);
+		System.out.println(secondCharacter);
+		
+		String word="Alura online courses";
+		System.out.println(word);
+		word=word+" 2023";
+		System.out.println(word);
+
+	}
+}
