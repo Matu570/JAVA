90d24652a01bede765d97575e4e722373fc6e775
Matias <matiasbueno@hotmail.com.ar>
2023-02-21 20:31:30 -0300

    Multiplication Table

---
+++ Java-2/Java-First-Steps/src/MultiplicationTable.java
@@ -0,0 +1,10 @@
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		for(int table=1;table<=10;table++) {
+			for(int multiplier=1;multiplier<=10;multiplier++) {
+				System.out.println(table + " X " + multiplier + " = " + (table*multiplier));
+			}
+		}
+	}
+}
