b368ab1eae234fd680052701e7458b72e9edc28b
Matias <matiasbueno@hotmail.com.ar>
2023-02-21 21:40:17 -0300

    Creating arrays

---
+++ Java-2/Java-First-Steps/src/Matrix.java
@@ -0,0 +1,16 @@
+
+public class Matrix {
+	public static void main(String[] args) {
+		
+		for (int row=0;row<=10;row++) {
+			for(int column=0;column<row;column++) {
+//				if(column>row) {
+//					break;
+//				}
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
